133116a840cb21a249ba6516ca679e8ef05fb27d
ArjunThazhath <146722372+arjunthazath@users.noreply.github.com>
2024-08-18 11:18:10 +0530

    new

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,31 +11,31 @@ function Home() {
         <div className='flex justify-center'>
           <div className='mr-7 mt-7 flex flex-col'>
 
-            <button className='bg-black rounded-xl w-[80px] h-[84px] flex flex-col items-center mb-3'>
-              <span className='text-gray-300 mb-1'>
+            <button className='bg-black rounded-xl w-[80px] h-[84px] flex flex-col items-center mb-3 text-gray-300 transform transition hover:scale-105  hover:text-yellow-400 duration-300'>
+              <span className=' mb-1'>
                 <span className='text-5xl'>+</span><br/>
-                <span className='text-gray-500 font-semibold'>Receive</span>
+                <span className='font-semibold'>Receive</span>
               </span>
             </button>
 
-            <button className='bg-black rounded-xl w-[80px] h-[84px] flex flex-col items-center mb-3'>
-              <span className='text-gray-300 mb-1 mt-2'>
+            <button className='bg-black rounded-xl w-[80px] h-[84px] flex flex-col items-center mb-3 text-gray-300 transform transition hover:scale-105  hover:text-yellow-400 duration-300'>
+              <span className='mb-1 mt-2'>
                 <span className='material-icons text-4xl'>send</span><br/>
-                <span className='text-gray-500 font-semibold'>Send</span>
+                <span className='font-semibold'>Send</span>
               </span>
             </button>
 
-            <button className='bg-black rounded-xl w-[80px] h-[84px] flex flex-col items-center mb-3'>
-              <span className='text-gray-300 mb-1 mt-2'>
+            <button className='bg-black rounded-xl w-[80px] h-[84px] flex flex-col items-center mb-3 text-gray-300 transform transition hover:scale-105  hover:text-yellow-400 duration-300'>
+              <span className='mb-1 mt-2'>
                 <span className='material-icons text-4xl'>swap_horiz</span><br/>
-                <span className='text-gray-500 font-semibold'>Swap</span>
+                <span className='font-semibold'>Swap</span>
               </span>
             </button>
 
-            <button className='bg-black rounded-xl w-[80px] h-[84px] flex flex-col items-center'>
-              <span className='text-gray-300 mb-1 mt-2'>
+            <button className='bg-black rounded-xl w-[80px] h-[84px] flex flex-col items-center text-gray-300 transform transition hover:scale-105  hover:text-yellow-400 duration-300'>
+              <span className='mb-1 mt-2'>
                 <span className='material-icons text-4xl'>attach_money</span><br/>
-                <span className='text-gray-500 font-semibold'>Buy</span>
+                <span className='font-semibold'>Buy</span>
               </span>
             </button>
 
@@ -44,7 +44,7 @@ function Home() {
         <div className='w-96 flex flex-col justify-center items-center text-6xl h-28 mt-5 mb-1 font-semibold'>$5.00</div>
         
           <div className='w-[800px] h-64'>
-            <div className='h-16 my-5 bg-custom-gray rounded-2xl flex flex-row justify-between items-center'>
+            <div className='h-16 my-5 bg-custom-gray rounded-2xl flex flex-row justify-between items-center transform transition hover:scale-105 duration-300'>
               <div className='flex flex-row'>
               <img className='h-9 my-2 mx-4' src={sol}/>
               <div className='mb-2'>
@@ -55,7 +55,7 @@ function Home() {
               </div>
               <div className='mr-5 text-lg font-semibold'>$0.00</div>
             </div>
-            <div className='h-16 my-5 bg-custom-gray rounded-2xl flex flex-row justify-between items-center'>
+            <div className='h-16 my-5 bg-custom-gray rounded-2xl flex flex-row justify-between items-center transform transition hover:scale-105 duration-300'>
               <div className='flex flex-row'>
               <img className='h-9 my-2 mx-4' src={btc}/>
               <div className='mb-2'>
@@ -66,7 +66,7 @@ function Home() {
               </div>
               <div className='mr-5 text-lg font-semibold'>$0.00</div>
             </div>
-            <div className='h-16 my-5 bg-custom-gray rounded-2xl flex flex-row justify-between items-center'>
+            <div className='h-16 my-5 bg-custom-gray rounded-2xl flex flex-row justify-between items-center transform transition hover:scale-105 duration-300'>
               <div className='flex flex-row'>
               <img className='h-9 my-2 mx-4' src={eth}/>
               <div className='mb-2'>
